0f20739021168fe32969db9c19a9d7539cef76ed
pipeac <34586557+pipeac@users.noreply.github.com>
2021-05-12 20:42:57 +0200

    Moviment i rotació amb smooth
    El Koala se puede mover en los ejes que elija, además tiene incorporada la rotación.
    Presenta el fallo de mantener los ojos en la direcció que le pertoca, después de haber hecho el giro, vuelve a su posición inicial

--- Assets/Script/Move_Koala.cs
+++ Assets/Script/Move_Koala.cs
@@ -6,7 +6,12 @@ public class Move_Koala : MonoBehaviour
 
     public float horizontalMove;
     public float verticalMove;
-    public float speed;
+
+    public float speed = 1.0f; //se ha hardcodeado, ya que unity lo pone por defecto en 0, y asi nos evitamos el error de speed = 0
+
+    public float turnSmootTime = 0.1f;
+    float turnSmoothVelocity; //velocidad al girar el personaje
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +23,27 @@ public class Move_Koala : MonoBehaviour
     void Update()
     {
 
+        float targetAngle = Mathf.Atan2(horizontalMove,verticalMove)*Mathf.Rad2Deg; //angulo que indica los grados de giro en un movimiento
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmootTime);
+
         horizontalMove = Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
 
+        
 
-        if (Input.GetKey(KeyCode.LeftArrow)) gameObject.transform.Translate(horizontalMove*speed, 0.0f, 0.0f);
+        //Swithc case de los movimientos en el planol
+        if (Input.GetKey(KeyCode.LeftArrow)) gameObject.transform.Translate(horizontalMove * speed * Time.deltaTime, 0.0f, 0.0f);
 
-        if (Input.GetKey(KeyCode.RightArrow)) gameObject.transform.Translate(horizontalMove * speed, 0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.RightArrow)) gameObject.transform.Translate(horizontalMove * speed * Time.deltaTime, 0.0f, 0.0f);
 
-        if (Input.GetKey(KeyCode.UpArrow)) gameObject.transform.Translate(0.0f, 0.0f, verticalMove * speed);
+        if (Input.GetKey(KeyCode.UpArrow)) gameObject.transform.Translate(0.0f, 0.0f, verticalMove * speed * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.DownArrow)) gameObject.transform.Translate(0.0f, 0.0f, verticalMove * speed);
+        if (Input.GetKey(KeyCode.DownArrow)) gameObject.transform.Translate(0.0f, 0.0f, verticalMove * speed * Time.deltaTime);
+
+
+        //Configuración del angulo de rotación
+        gameObject.transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
     }
 }

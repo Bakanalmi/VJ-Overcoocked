5b3b26982453c496c28cf5d384ee1abdb0268ab0
Bakanalmi <marc2almi@gmail.com>
2021-05-24 00:27:45 +0200

    New assets + fixed bugs
    Encarà hi ha bastants bugs amb el menjar i falta fer el menú

---
+++ Assets/Script/CrateAction.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateAction : MonoBehaviour
+{
+    public GameObject lid;
+
+    public Transform open;
+    public Transform close;
+
+    // Start is called before the first frame update
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            OpenLid(close);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            OpenLid(open);
+    }
+
+    private void OpenLid(Transform place)
+    {
+        // Set Slot as a parent
+        lid.transform.SetParent(place);
+
+        // Reset position and rotation
+        lid.transform.localPosition = Vector3.zero;
+        lid.transform.localRotation = Quaternion.identity;
+    }
+
+    private void CloseLid(Transform place)
+    {
+        // Set Slot as a parent
+        lid.transform.SetParent(place);
+
+        // Reset position and rotation
+        lid.transform.localPosition = Vector3.zero;
+        lid.transform.localRotation = Quaternion.identity;
+    }
+}

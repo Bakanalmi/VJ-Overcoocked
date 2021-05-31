4872a7d26a335714d67c6a7ed5c12812af3f4dfb
pipeac <34586557+pipeac@users.noreply.github.com>
2021-05-31 04:42:07 +0200

    Plantilla de comanda
    La plantilla de la comanda ja esta implementada, cal que iteraccioni amb la escena

--- Assets/Script/Escena/MainMenu.cs
+++ Assets/Script/Escena/MainMenu.cs
@@ -1,15 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Button PlayButton;
+    public Button Scenen1;
+    public Button Scenen2;
+    public int indexScene;
+
+
+    public void Start()
+    {
+        indexScene = 1;
+        
+        Scenen1.onClick.AddListener(delegate { TaskOnClick(1); });
+        Scenen2.onClick.AddListener(delegate { TaskOnClick(2); });
+
+    }
+  
+
+    private void TaskOnClick(int index)
+    {
+        indexScene = index;
+    }
+
     public void SwapScene()
     {
+        
         //SceneManager.LoadScene("SampleScene"); 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        //SceneManager.LoadScene();
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(indexScene);
     }
 
     public void QuitGame()
